4a0bd56b292d497787680368d619b548230f78fe
HoangMinh <starsrising8888@gmail.com>
2025-06-28 11:08:10 +0700

    update

---
+++ workspace_I2C/I2C_Slave_C8T6/Core/Inc/i2c_slave.h
@@ -0,0 +1,14 @@
+/*
+ * i2c_slave.h
+ *
+ *  Created on: May 4, 2025
+ *      Author: TS
+ */
+
+#ifndef INC_I2C_SLAVE_H_
+#define INC_I2C_SLAVE_H_
+
+extern uint8_t I2C_REGISTERS[10];
+extern I2C_HandleTypeDef hi2c1;
+
+#endif /* INC_I2C_SLAVE_H_ */
